3786aea4d4d1d1e696fb53cbe2dfea9689436966
Sebastian T F <sebastian.tharakan97@gmail.com>
2022-08-26 17:25:19 +0530

    use strum enum for ConfigKeys

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,5 +1,11 @@
 use dotenv::dotenv;
 
+#[derive(strum::Display)]
+pub enum ConfigKey {
+    #[strum(serialize = "ETH_RPC_URL")]
+    EthRpcUrl,
+}
+
 #[derive(Debug)]
 pub struct Config {
     pub eth_provider_rpc: String,
@@ -8,7 +14,8 @@ pub struct Config {
 impl Default for Config {
     fn default() -> Self {
         dotenv().ok();
-        let eth_provider_rpc = dotenv::var("ETH_RPC_URL").expect("No ETH_RPC_URL");
+        let eth_provider_rpc = dotenv::var(ConfigKey::EthRpcUrl.to_string())
+            .expect(&format!("{} not found!", ConfigKey::EthRpcUrl.to_string()));
 
         Config { eth_provider_rpc }
     }
