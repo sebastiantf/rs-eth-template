722c58e41283c8efd0386fb628b16a8f387d9c7e
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-27 08:52:32 +0530

    feat: ✨ use run()`

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+use ethers::prelude::*;
 use log::{debug, info};
 
 use rs_eth_template::config::Config;
 
-fn main() {
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
 
     debug!("Initializing Config...");
     let config = Config::default();
     info!("Config: {:#?}", &config);
 
-    rs_eth_template::run();
+    debug!("Initializing Provider...");
+    let provider = Provider::<Http>::try_from(&config.eth_provider_rpc)?;
+
+    rs_eth_template::run(&config, provider).await?;
+
+    Ok(())
 }
