49a540c1b936d6fe025c65507a0a486c75138ca6
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-27 08:52:13 +0530

    feat: ✨ get latest block number in `run()`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 pub mod config;
 
+use config::Config;
+use ethers::prelude::*;
 use log::info;
 
-pub fn run() {
-    info!("Hello, world!");
+pub async fn run<P: JsonRpcClient>(_config: &Config, provider: Provider<P>) -> eyre::Result<()> {
+    let latest_block_number = provider.get_block_number().await?;
+    info!("Latest block: {}", latest_block_number);
+    Ok(())
 }
